2f0450d6de453ec23008ada7828739b20f288214
T-Frick <toby.frick@hotmail.com>
2024-02-22 21:19:25 -0600

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <div className='headerBox'>
+      <div className='headerTile'>1</div>
+      <div className='headerTile'>2</div>
+      <div className='headerTile'>3</div>
+      <div className='headerTile'>4</div>
+    </div>
+  )
+}
+
+export default Header
